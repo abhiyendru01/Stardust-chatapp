4c645e55c59395dc2a6a0b3257c2d39420c0cc99
abhiyendru <rahul.r0644@gmail.com>
2025-03-12 21:17:46 +0530

    commit

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,9 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "production"
-  ? "http://localhost:5001"
-  : "https://fullstack-chat-app-master-j115.onrender.com";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5001";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,

43fb9e82201635ae8ae091cd34855502516f0e29
abhiyendru <rahul.r0644@gmail.com>
2025-03-17 03:42:03 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
+dotenv.config();
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 500,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor";
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
-});
+    sourcemap: true,
+  }
+})
